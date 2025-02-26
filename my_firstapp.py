bedb4eb58b1fd98f420a30bf5d4d6996149bdc0d
3hhd9 <ahadazzan9@gmail.com>
2025-02-26 11:04:30 +0400

    Added all files

---
+++ my_firstapp.py
@@ -0,0 +1,84 @@
+def generate_multiplication_table(number, range_values):
+    
+    for i in range_values:
+        print(f"{number} x {i} = {number * i}")
+
+number = int(input("Enter a number to generate  multiplication table: "))
+
+
+custom_range = input("Do you want to specify a custom range (yes/no)? ").lower()
+
+if custom_range == 'yes':
+    starting = int(input("Enter  starting number : "))
+    ending = int(input("Enter the ending number : "))
+    range_values = list(range(starting, ending + 1)) 
+    
+    generate_multiplication_table(number, range_values)
+else:
+    range_values = list(range(1, 11))  
+    generate_multiplication_table(number, range_values)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
